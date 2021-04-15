9f49f99a694ebf6d5007ec7cf0dec723d62f6bd7
kakivasantha <kakivasanthareddy@gmail.com>
2021-04-15 13:26:49 +0530

    add isDivisible functionality

--- main.go
+++ main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/kakivasantha/learning/problems"
 	"github.com/kakivasantha/learning/test"
 )
 
 func main() {
-	fmt.Println("hello world", test.Name)
+	fmt.Println("welcome ", test.Name)
+	arg := "nothing"
+	if len(os.Args) > 1 {
+		arg = os.Args[1]
+	}
+	switch arg {
+	case "isDivisible":
+		var value, divider int
+		fmt.Println("value: ")
+		fmt.Scan(&value)
+		fmt.Println("divider: ")
+		fmt.Scan(&divider)
+		if problems.IsDivisible(value, divider) {
+			fmt.Printf("%v can be divisible by %v\n", value, divider)
+		} else {
+			fmt.Printf("%v cannot be divisible by %v\n", value, divider)
+		}
+	case "nothing":
+		fmt.Println("no commands to run")
+	default:
+		fmt.Println("command does not exist")
+	}
+	fmt.Println("see you ", test.Name)
+
 }
