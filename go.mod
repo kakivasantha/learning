7ffd1dc874621e0e805825bde3bf549cc7019ddf
kakivasantha <kakivasanthareddy@gmail.com>
2021-04-15 12:18:55 +0530

    setting up project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kakivasantha/learning
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kakivasantha/learning/test"
+)
+
+func main() {
+	fmt.Println("hello world", test.Name)
+}
